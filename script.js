4c8d3eba08e6be32e324b3269617856050e9e860
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-04-01 20:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function randomDelay() {
+  return Math.floor(Math.random() * 3) + 1;
+}
+
+const promises = [
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 1'), randomDelay() * 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 2'), randomDelay() * 1000);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => resolve('Promise 3'), randomDelay() * 1000);
+  }),
+];
+
+Promise.all(promises)
+  .then((results) => {
+    const output = document.querySelector('#output');
+    output.innerHTML = '';
+    let totalTime = 0;
+    results.forEach((result, i) => {
+      const time = randomDelay();
+      output.innerHTML += `
+        <tr>
+          <td>${result}</td>
+          <td>${time}</td>
+        </tr>
+      `;
+      totalTime += time;
+    });
+    output.innerHTML += `
+      <tr>
+        <td>Total</td>
+        <td>${totalTime.toFixed(3)}</td>
+      </tr>
+    `;
+  });
